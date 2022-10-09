54bb38f0b6771eaad4abe04f5a654755bccb44f7
Agil Rajayyan <66911936+AgilRajayyan@users.noreply.github.com>
2022-10-09 15:22:27 +0530

    Initial cleanup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={classes.App}>
+      <header className={classes['App-header']}>
+        <p>Stay tuned!</p>
       </header>
     </div>
   );
