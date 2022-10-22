fe734ac364e1c755d34e821b57c85927a4676f76
Agil Rajayyan <66911936+AgilRajayyan@users.noreply.github.com>
2022-10-22 12:57:02 +0530

    -Initialized firebase analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import classes from './App.module.css';
+import { analytics } from './index';
+import { logEvent } from 'firebase/analytics';
 
 function App() {
+  const clickHandler = () => {
+    logEvent(analytics, 'stay_tuned_clicked');
+  };
+
   return (
     <div className={classes.App}>
       <header className={classes['App-header']}>
-        <p>Stay tuned!</p>
+        <p onClick={clickHandler}>Stay tuned!</p>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCFY2AiOiV9jTaHml9s7TC8IM2BRFgZrVk',
+  authDomain: 'slayinterest.firebaseapp.com',
+  projectId: 'slayinterest',
+  storageBucket: 'slayinterest.appspot.com',
+  messagingSenderId: '88321934137',
+  appId: '1:88321934137:web:9d65f52a357dea14351ee8',
+  measurementId: 'G-TQWYBM5PSG',
+};
+const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
